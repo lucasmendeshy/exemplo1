a706649d90b1cce1fab677be687fcede6645cddb
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-04 09:59:36 -0300

    estilos, import e testes

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={estilos.container}>
+      <Text style={estilos.container}>Olá!</Text>
+    </View>
+  );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textTransform: "Uppercase",
+    color: "red",
+    backgroundColor: "black",
+  },
+});
